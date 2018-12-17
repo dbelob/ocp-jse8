d59143aeeaca3ed6fef30df7b3e80bf2b39b8f74
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-12-17 20:12:16 +0300

    Add test examples

---
+++ src/main/java/acme/study_guide/chapter1/access_modifiers/mouse/Mouse.java
@@ -0,0 +1,13 @@
+package acme.study_guide.chapter1.access_modifiers.mouse;
+
+import acme.study_guide.chapter1.access_modifiers.cat.BigCat;
+
+public class Mouse {
+    public static void main(String[] args) {
+        BigCat cat = new BigCat();
+        System.out.println(cat.name);
+//        System.out.println(cat.hasFur);
+//        System.out.println(cat.hasPaws);
+//        System.out.println(cat.id);
+    }
+}
