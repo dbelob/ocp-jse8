0cbe273517a50a420684cdf55c55a374c01c9586
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-12-19 19:43:57 +0300

    Add test examples

---
+++ src/main/java/acme/study_guide/chapter1/working_with_enums/Example.java
@@ -0,0 +1,24 @@
+package acme.study_guide.chapter1.working_with_enums;
+
+public class Example {
+    public static void main(String[] args) {
+        for (Season season : Season.values()) {
+            System.out.println(season.name() + " " + season.ordinal());
+        }
+
+        Season s1 = Season.valueOf("SUMMER"); // SUMMER
+//        Season s2 = Season.valueOf("summer"); // exception
+
+        Season summer = Season.SUMMER;
+        switch (summer) {
+            case WINTER:
+                System.out.println("Get out the sled!");
+                break;
+            case SUMMER:
+                System.out.println("Time for the pool!");
+                break;
+            default:
+                System.out.println("Is it summer yet?");
+        }
+    }
+}
