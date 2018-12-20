59c7192b5127be3977a1b8359ac89e9964535ddc
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-12-20 20:52:50 +0300

    Add test examples

---
+++ src/main/java/acme/study_guide/chapter1/creating_nested_classes/anonymous_inner_classes/AnonInner.java
@@ -0,0 +1,16 @@
+package acme.study_guide.chapter1.creating_nested_classes.anonymous_inner_classes;
+
+public class AnonInner {
+    abstract class SaleTodayOnly {
+        abstract int dollarsOff();
+    }
+
+    public int admission(int basePrice) {
+        SaleTodayOnly sale = new SaleTodayOnly() {
+            int dollarsOff() {
+                return 3;
+            }
+        };
+        return basePrice - sale.dollarsOff();
+    }
+}
