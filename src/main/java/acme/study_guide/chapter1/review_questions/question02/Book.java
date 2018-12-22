993ee42063563ac62853e6a326b79e6f9e559aa3
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-12-22 17:16:53 +0300

    Add questions

---
+++ src/main/java/acme/study_guide/chapter1/review_questions/question02/Book.java
@@ -0,0 +1,17 @@
+package acme.study_guide.chapter1.review_questions.question02;
+
+public class Book {
+    private int ISBN;
+    private String title, author;
+    private int pageCount;
+    public int hashCode() {
+        return ISBN;
+    }
+    @Override public boolean equals(Object obj) {
+        if ( !(obj instanceof Book)) {return false;
+        }
+        Book other = (Book) obj;
+        return this.ISBN == other.ISBN;
+    }
+    // imagine getters and setters are here
+}
