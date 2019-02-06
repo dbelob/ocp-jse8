5d88dff9d38e637d73853ab346213a6f2e45b421
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-02-06 19:42:47 +0300

    Add test examples

---
+++ src/main/java/acme/study_guide/chapter05/adding_internationalization_and_localization/Example.java
@@ -0,0 +1,10 @@
+package acme.study_guide.chapter05.adding_internationalization_and_localization;
+
+import java.util.Locale;
+
+public class Example {
+    public static void main(String[] args) {
+        Locale locale = Locale.getDefault();
+        System.out.println(locale);
+    }
+}
