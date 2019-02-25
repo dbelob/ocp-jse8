ccc10805d099e7344b585b8fc6104cf771777a61
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-02-25 19:34:04 +0300

    Add test examples

---
+++ src/main/java/acme/study_guide/chapter07/synchronizing_data_access/SheepManager.java
@@ -0,0 +1,25 @@
+package acme.study_guide.chapter07.synchronizing_data_access;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SheepManager {
+    private int sheepCount = 0;
+
+    private void incrementAndReport() {
+        System.out.print((++sheepCount) + " ");
+    }
+
+    public static void main(String[] args) {
+        ExecutorService service = null;
+        try {
+            service = Executors.newFixedThreadPool(20);
+
+            SheepManager manager = new SheepManager();
+            for (int i = 0; i < 10; i++)
+                service.submit(() -> manager.incrementAndReport());
+        } finally {
+            if (service != null) service.shutdown();
+        }
+    }
+}
