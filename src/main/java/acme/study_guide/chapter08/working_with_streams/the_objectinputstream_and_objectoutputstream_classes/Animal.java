1b831ac561d6f751f0a3dae0afe142cb34202292
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-03-12 20:14:07 +0300

    Add test examples

--- src/main/java/acme/study_guide/chapter08/working_with_streams/the_objectinputstream_and_objectoutputstream_classes/Animal.java
+++ src/main/java/acme/study_guide/chapter08/working_with_streams/the_objectinputstream_and_objectoutputstream_classes/Animal.java
@@ -3,10 +3,18 @@ package acme.study_guide.chapter08.working_with_streams.the_objectinputstream_an
 import java.io.Serializable;
 
 public class Animal implements Serializable {
-    private static final long serialVersionUID = 1L;
-    private String name;
-    private int age;
-    private char type;
+    private static final long serialVersionUID = 2L;
+    private transient String name;
+    private transient int age = 10;
+    private static char type = 'C';
+
+    {this.age = 14;}
+
+    public Animal() {
+        this.name = "Unknown";
+        this.age = 12;
+        this.type = 'Q';
+    }
 
     public Animal(String name, int age, char type) {
         this.name = name;
