d326b84924d04bb47c8555b9821fef739330920e
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-04-13 23:43:12 +0300

    Add test examples

---
+++ src/main/java/acme/study_guide/assessment/test04/Box.java
@@ -0,0 +1,20 @@
+package acme.study_guide.assessment.test04;
+
+public class Box<T> {
+    T value;
+
+    public Box(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public static void main(String[] args) {
+        Box<String> one = new Box<String>("a string");
+        Box<Integer> two = new Box<>(123);
+        System.out.print(one.getValue());
+        System.out.print(two.getValue());
+    }
+}
