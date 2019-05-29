465c1098381c28c59c040f21dd961b62a31f1ddb
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-05-29 20:39:00 +0300

    Add test examples

---
+++ src/main/java/acme/enthuware/standard_tests/foundation_test/test28/TestClass.java
@@ -0,0 +1,18 @@
+package acme.enthuware.standard_tests.foundation_test.test28;
+
+public class TestClass
+{
+    static int si = 10;   int ii = 20;
+    public void inner()
+    {
+        int ai = 30; //automatic variable
+        ai = 31;//ai is not effectively final anymore.
+        final int fai = 40; //automatic final variable
+        class Inner
+        {
+            public Inner()  {  System.out.println(si+"  "+ii+"   "+fai);  }
+        }
+        new Inner();
+    }
+    public static void main(String[] args){  new TestClass().inner();  }
+}
