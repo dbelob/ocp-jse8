bd89afd14c9974681c2e853335f926b1e17e79a6
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-06-02 23:56:35 +0300

    Add test examples

---
+++ src/main/java/acme/enthuware/standard_tests/test2/Example.java
@@ -0,0 +1,12 @@
+package acme.enthuware.standard_tests.test2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Example {
+    public static void main(String[] args) {
+        List<String> vals = Arrays.asList("a", "b");
+        String join = vals.parallelStream().reduce("_", (a, b) -> a.concat(b));
+        System.out.println(join);
+    }
+}
