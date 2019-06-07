98f3aa0ad345aea7c7d734a9588952d2533266b6
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-06-07 13:54:47 +0300

    Add test examples

---
+++ src/main/java/acme/enthuware/objective_wise_tests/chapter11_concurrency/TestClass.java
@@ -0,0 +1,22 @@
+package acme.enthuware.objective_wise_tests.chapter11_concurrency;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+class MyCallable implements Callable<String> {
+    public String call() throws Exception {
+        Thread.sleep(50000);
+        return "DONE";
+    }
+}
+
+public class TestClass {
+    public static void main(String[] args) throws Exception {
+        ExecutorService es = Executors.newSingleThreadExecutor();
+        Future<String> future = es.submit(new MyCallable());
+        System.out.println(future.get()); //1
+        es.shutdownNow(); //2
+    }
+}
